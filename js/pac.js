165b0dce1776be672c73611ca4eba274660a8b81
ISLAM MD SHARIFUL <61687175+sinoyon@users.noreply.github.com>
2020-12-27 04:56:55 +0800

    Add files via upload

---
+++ js/pac.js
@@ -0,0 +1,73 @@
+
+    document.getElementById("but").onclick = function() {myF()};
+
+    function myF() {
+      document.getElementById("ja").innerHTML = "3 DAYS 2 NIGHTS RM 287";
+        document.getElementById("j").innerHTML = "5 DAYS 4 NIGHT RM 699";
+      document.getElementById("ja").style.fontSize = "17px";
+      document.getElementById("ja").style.color = "#008080";
+      document.getElementById("j").style.fontSize = "17px";
+      document.getElementById("j").style.color = "#008080";
+
+
+    };
+    document.getElementById("bu").onclick = function() {myFa()};
+
+    function myFa() {
+      document.getElementById("a").innerHTML = "2 DAYS 1 NIGHT RM 88";
+        document.getElementById("b").innerHTML = "3 DAYS 2 NIGHTS RM 299";
+
+        document.getElementById("a").style.fontSize = "17px";
+        document.getElementById("a").style.color = "#008080";
+        document.getElementById("b").style.fontSize = "17px";
+        document.getElementById("b").style.color = "#008080";
+
+    };
+    document.getElementById("bc").onclick = function() {myFc()};
+
+    function myFc() {
+      document.getElementById("jc").innerHTML = "3 DAYS 2 NIGHTS RM 813";
+        document.getElementById("c").innerHTML = "4 DAYS 3 NIGHT RM 999";
+
+        document.getElementById("jc").style.fontSize = "17px";
+        document.getElementById("jc").style.color = "#008080";
+        document.getElementById("c").style.fontSize = "17px";
+        document.getElementById("c").style.color = "#008080";
+
+    };
+    document.getElementById("bd").onclick = function() {myFd()};
+
+    function myFd() {
+      document.getElementById("jd").innerHTML = "3 DAYS 2 NIGHTS RM 287";
+        document.getElementById("d").innerHTML = "5 DAYS 4 NIGHT RM 699";
+
+        document.getElementById("jd").style.fontSize = "17px";
+        document.getElementById("jd").style.color = "#008080";
+        document.getElementById("d").style.fontSize = "17px";
+        document.getElementById("d").style.color = "#008080";
+
+    };
+    document.getElementById("bx").onclick = function() {myFx()};
+
+    function myFx() {
+      document.getElementById("jx").innerHTML = "1 DAY SPECIAL RM 199";
+        document.getElementById("x").innerHTML = "2 DAYS 1 NIGHT RM 299";
+
+        document.getElementById("jx").style.fontSize = "17px";
+        document.getElementById("jx").style.color = "#008080";
+        document.getElementById("x").style.fontSize = "17px";
+        document.getElementById("x").style.color = "#008080";
+
+    };
+    document.getElementById("bz").onclick = function() {myFz()};
+
+    function myFz() {
+      document.getElementById("jz").innerHTML = "2 DAYS 1 NIGHT RM 299";
+        document.getElementById("z").innerHTML = "3 DAYS 2 NIGHTS RM 449";
+
+        document.getElementById("jz").style.fontSize = "17px";
+        document.getElementById("jz").style.color = "#008080";
+        document.getElementById("z").style.fontSize = "17px";
+        document.getElementById("z").style.color = "#008080";
+
+    };
